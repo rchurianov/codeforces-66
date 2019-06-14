69cac072a90f388c150799d22555b39e6f1b5a46
Roman Churyanov <Roman.Churyanov@redsys.ru>
2019-06-14 19:21:07 +0300

    Overflow class.

--- src/com/roman/Overflow.java
+++ src/com/roman/Overflow.java
@@ -4,26 +4,62 @@ import java.util.Scanner;
 
 public class Overflow {
 
+    /*
+    Notes:
+    Boundaries:
+    x in [0, 2^32 - 1]
+
+    int type: [ -2^32, 2^32 - 1 ]
+    x should fit in integer type
+    thus we can catch java Exception on integer overflow
+    or just hardcode a constant and compare current value with it
+    or we can use an existing constant from Integer wrapper class
+    2^32 = 4294967296
+    2^32 - 1 = 4294967296 - 1
+
+    initial value
+    x = 0
+
+    add
+    ++x
+
+    for 35
+    end
+     */
+
+    private int maximum_integer = Integer.MAX_VALUE;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long x = 0;
-        for (int i = 0; i < n; ++i) {
-            Cycle(sc);
+        int x = 0;
+        while (sc.hasNext()) {
+            x = Cycle(sc, x);
         }
+        System.out.println(x);
     }
 
-    private static void Cycle(Scanner sc) {
+    private static int Cycle(Scanner sc, int x) {
         String line = sc.nextLine();
+
+        // bottom case
+        if (line.equals("end")) {
+            return x;
+        }
+
+        if (line.equals("add")) {
+            ++x;
+        }
+
         // or use recursion
         if (line.contains("for")) {
             String[] tokens = line.split(" ");
-            int m = Integer.getInteger(tokens[1]);
+            int m = Integer.parseInt(tokens[1]);
             for (int j = 0; j < m; ++j) {
-                // recursions call
-                Cycle(sc);
-                // how to specify bottom out case
+                x = Cycle(sc, x);
             }
         }
+
+        return x;
     }
 }
